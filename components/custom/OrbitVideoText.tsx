188a71197cfe494fe5ab72ee5058b393241a7518
su69ar <rdna.connection@gmail.com>
2025-07-09 22:18:48 +0800

    upd: orbit mobile

--- components/custom/OrbitVideoText.tsx
+++ components/custom/OrbitVideoText.tsx
@@ -15,7 +15,7 @@ export default function OrbitVideoText() {
       ></div>
 
       <div className="relative z-10 grid grid-cols-1 md:grid-cols-2 gap-10 items-center max-w-7xl mx-auto">
-        <div className="flex justify-center">
+        <div className="flex justify-center sm:ml-[-20px]">
           <OrbitingProcess />
         </div>
         <div className="flex justify-center">
