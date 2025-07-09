7b8eabf867d3e88ffe87ab0a2f46e9d3b865b42f
su69ar <rdna.connection@gmail.com>
2025-07-09 20:47:33 +0800

    update: site title

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,9 @@ import './globals.css'
 import './fonts.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'ZBIO',
+  description: "Bali's First Circular Food Ecosystem",
+  generator: 'Bali Micro Technology',
 }
 
 export default function RootLayout({
