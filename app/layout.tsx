beeabd922e19bfa3b8fc524ed76800df7f2785c5
su69ar <rdna.connection@gmail.com>
2025-07-21 14:26:16 +0800

    add: favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,24 @@
+// app/layout.tsx
 import type { Metadata } from 'next'
 import './globals.css'
-import './fonts.css'
 
 export const metadata: Metadata = {
   title: 'ZBIO',
   description: "Bali's First Circular Food Ecosystem",
   generator: 'Bali Micro Technology',
+  icons: {
+    icon: './favicon.png', // Path ke favicon Anda
+    apple: './favicon.png',
+  },
 }
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode
-}>) {
+}) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body>{children}</body>
     </html>
   )
