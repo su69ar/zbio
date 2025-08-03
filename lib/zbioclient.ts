cb2ed6c939b21477ce852aaac779f1548b762919
su69ar <rdna.connection@gmail.com>
2025-08-03 11:34:14 +0800

    fix: wirasana logo

--- lib/zbioclient.ts
+++ lib/zbioclient.ts
@@ -1,7 +1,7 @@
 export const hotels = [
   { name: 'Amarta', imageUrl: '/client/hotels/amarta.png' },
   { name: 'Kempinski', imageUrl: '/client/hotels/kempinski.png' },
-  { name: 'Wirasana Inn', imageUrl: '/client/hotels/Wirasana.png' },
+  { name: 'Wirasana Inn', imageUrl: '/client/hotels/wirasana.png' },
   { name: 'Keramas', imageUrl: '/client/hotels/keramas.png' },
   { name: 'Lv8', imageUrl: '/client/hotels/lv8.png' },
   { name: 'Novotel', imageUrl: '/client/hotels/novotel.png' },
