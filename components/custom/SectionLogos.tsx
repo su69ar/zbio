4f54bd46eee5c0bf54df04fb4c1e7ea1464b2ca6
su69ar <rdna.connection@gmail.com>
2025-08-03 11:31:30 +0800

    fix: clent logo error and layout on mobile

--- components/custom/SectionLogos.tsx
+++ components/custom/SectionLogos.tsx
@@ -1,35 +1,37 @@
-import React from 'react';
+import React from 'react'
 
 interface LogoItem {
-  name: string;
-  imageUrl: string;
+  name: string
+  imageUrl: string
 }
 
 interface SectionLogosProps {
-  title: string;
-  logos: LogoItem[];
+  title: string
+  logos: LogoItem[]
 }
 
 const SectionLogos: React.FC<SectionLogosProps> = ({ title, logos }) => {
   return (
-    <section className="py-8">
+    <section className='py-8'>
       {/* Judul section */}
-      <h2 className="text-3xl font-bold mb-6 text-[#1F382B] text-center lg:text-left">{title}</h2>
+      <h2 className='text-3xl font-bold mb-6 text-[#1F382B] text-center lg:text-left'>
+        {title}
+      </h2>
 
       {/* Kontainer logo dalam grid */}
-      <div className="grid grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-8 place-items-center">
+      <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-8 place-items-center'>
         {logos.map((logo) => (
-          <div key={logo.name} className="w-32 flex justify-center">
+          <div key={logo.name} className='w-32 flex justify-center'>
             <img
               src={logo.imageUrl}
               alt={logo.name}
-              className="max-h-20 object-contain"
+              className='max-h-20 object-contain'
             />
           </div>
         ))}
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default SectionLogos;
+export default SectionLogos
--- lib/zbioclient.ts
+++ lib/zbioclient.ts
@@ -1,10 +1,10 @@
 export const hotels = [
   { name: 'Amarta', imageUrl: '/client/hotels/amarta.png' },
   { name: 'Kempinski', imageUrl: '/client/hotels/kempinski.png' },
-  { name: 'Wirasana Inn', imageUrl: '/client/hotels/keramas.png' },
-  { name: 'Keramas', imageUrl: '/client/hotels/lv8.png' },
-  { name: 'Lv8', imageUrl: '/client/hotels/novotel.png' },
-  { name: 'Novotel', imageUrl: '/client/hotels/Wirasana.png' },
+  { name: 'Wirasana Inn', imageUrl: '/client/hotels/Wirasana.png' },
+  { name: 'Keramas', imageUrl: '/client/hotels/keramas.png' },
+  { name: 'Lv8', imageUrl: '/client/hotels/lv8.png' },
+  { name: 'Novotel', imageUrl: '/client/hotels/novotel.png' },
   { name: 'Revivo', imageUrl: '/client/hotels/revivo.png' },
   { name: 'Grand Mencure', imageUrl: '/client/hotels/grand_mercure.png' },
   {
